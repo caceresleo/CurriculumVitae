f8cf7f7c123508c9f6bf64e537375640290d3f1e
Leonardo Caceres <caceresleonardo48@gmail.com>
2023-03-07 23:38:49 -0300

    cv finalizado

---
+++ js/contenidoscript.js
@@ -0,0 +1,50 @@
+var nombrePersona = document.getElementById('nombrePrincipal');
+var profesionPersona = document.getElementById('profesionPrincipal');
+var fotoPersona = document.getElementById('fotoCarnet');
+var edadPersona = document.getElementById('edad');
+var callePersona = document.getElementById('calle');
+var numeroPersona = document.getElementById('numero');
+var codigoPersona = document.getElementById('codigo');
+var estadoPersona = document.getElementById('estado');
+var emailPersona = document.getElementById('email');
+var telefonoPersona = document.getElementById('telefono');
+var wappPersona = document.getElementById('wapp');
+
+/*... secciones  .....*/
+var secciones = [
+    'conoc',
+    'cont',
+    'leng',
+    'expe',
+    'forma',
+    'sobre'
+];
+
+function seleccionSeccion(actual){
+    for(var i = 0; i < 6 ; i++){
+        var borrarSeccionActual = document.getElementById(secciones[i]);
+        borrarSeccionActual.style.display= "none";
+    }
+
+    var mostrarSeccion = document.getElementById(actual);
+    mostrarSeccion.style.display = "block"
+}
+
+
+fetch('https://randomuser.me/api/?format=json')
+    .then(x => x.text())
+    .then(y => {
+        var datosPersona = JSON.parse(y).results[0];
+        nombrePersona.innerHTML = datosPersona.name.title + " "+
+                                    datosPersona.name.first + " "+ datosPersona.name.last;
+        fotoPersona.src= datosPersona.picture.large;
+        edadPersona.innerHTML = datosPersona.dob.age;
+        callePersona.innerHTML = datosPersona.location.street.name;
+        numeroPersona.innerHTML = datosPersona.location.street.number;
+        codigoPersona.innerHTML = datosPersona.location.postcode;
+        estadoPersona.innerHTML = datosPersona.location.city + datosPersona.location.country;
+        emailPersona.innerHTML = datosPersona.email;
+        telefonoPersona.innerHTML = datosPersona.cell;
+        wappPersona.innerHTML = datosPersona.cell;
+
+         });
